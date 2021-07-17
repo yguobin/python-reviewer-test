f0f8ac2ea7f4f92c65e8bf12346bfa8f58ab3a2f
Richard Yin <richardyin@ubuntu20.04VM>
2021-07-17 12:33:01 -0400

    fixed

--- lab-2.py
+++ lab-2.py
@@ -1,11 +1,11 @@
 
-operations_count = 0
-
 def main():
     ask_again = True
+    operations_count = 0
     while(ask_again):
         a = input("Enter the numerator: ")
         b = input("Enter the denominator: ")
+        operations_count += 1
         result = perform_division(a,b)
         print(result)
         ask_again = input("Do you want to perform another operation? Enter yes or no: ")
@@ -13,16 +13,14 @@ def main():
             ask_again = True
         else:
             ask_again = False
-            print("You performed " + str(operations_count) + " operations, bye!")
+            print(f"You performed {operations_count} operations, bye!")
 
 
 def perform_division(a,b):
-    global operations_count
     try:
-        operations_count += 1
         return int(a)/int(b)
-    except Exception as e:
-        pass
+    except ZeroDivisionError as e:
+        return float("NAN")
 
 
 main()
